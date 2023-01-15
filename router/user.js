17fd4d1427a351683e19f33ff4f164ca8548e04b
Md Mamun <juborajvai22@gmail.com>
2023-01-15 19:40:09 +0600

    update

--- router/user.js
+++ router/user.js
@@ -1,15 +1,23 @@
-const express = require('express')
-const { registerUser, loginUser, getAllUser, singleUser, deleteUser, cheackAdmin, createAdmin,  } = require('../controler/userControler')
-const verifayToken = require('../utilitis/verifayToken')
-const router = express.Router()
+const express = require("express");
+const {
+  registerUser,
+  loginUser,
+  getAllUser,
+  singleUser,
+  deleteUser,
+  cheackAdmin,
+  createAdmin,
+} = require("../controler/userControler");
+const verifayToken = require("../utilitis/verifayToken");
+const router = express.Router();
 
-// import all controler 
-router.post("/register" , registerUser)
-router.post("/login" , loginUser)
-router.get("/user" , getAllUser)
-router.get("/user/:id" , singleUser)
-router.delete("/user/:id" , deleteUser)
-router.get("/chackAdmin/:email" , verifayToken, cheackAdmin)
-router.put("/admin/:email" , createAdmin);
+// import all controler
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/user", getAllUser);
+router.get("/user/:id", singleUser);
+router.delete("/user/:id", deleteUser);
+router.get("/chackAdmin/:email", cheackAdmin);
+router.put("/admin/:email", createAdmin);
 
-module.exports = router
+module.exports = router;
